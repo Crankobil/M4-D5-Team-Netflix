eb561e692f5de532eac0ef7cac048cdbf1a91322
foram patel <p.foram147@gmail.com>
2021-08-27 18:54:21 +0530

    logo left
    navbar nd footer done

---
+++ m4-d5-team-netflix/src/components/MyNavbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import {Navbar,Nav} from 'react-bootstrap'
+import { Bell, Search} from 'react-bootstrap-icons';
+import Image from 'react-bootstrap/Image'
+
+export default function MyNavbar(){
+    return(
+    <Navbar bg="dark" >
+      <Nav defaultActiveKey="/home" as="ul">
+        <Image
+             src="src/assets/netflix_logo.png"
+             width="80"
+             height="30"
+        />
+      <Nav.Item as="li">
+         <Nav.Link  href="/home" className="font-color">Home</Nav.Link>
+      </Nav.Item>
+      <Nav.Item as="li">
+          <Nav.Link eventKey="link-1" className="font-color">TV Shows</Nav.Link>
+      </Nav.Item>
+      <Nav.Item as="li">
+          <Nav.Link eventKey="link-2"className="font-color">Movies</Nav.Link>
+      </Nav.Item>
+      <Nav.Item as="li">
+          <Nav.Link eventKey="link-2"className="font-color">Recently Added</Nav.Link>
+      </Nav.Item>
+      <Nav.Item as="li">
+          <Nav.Link eventKey="link-2"className="font-color">My List</Nav.Link>
+      </Nav.Item>
+</Nav>
+<Nav as="ul" className="nav-left">
+      <Nav.Item as="li">
+         <Search className="mr-2" color="white" size={20}></Search>
+      </Nav.Item>
+      <Nav.Item as="li">
+         <Bell color="white" size={20}/>
+      </Nav.Item>
+  </Nav> 
+
+</Navbar>
+) }
